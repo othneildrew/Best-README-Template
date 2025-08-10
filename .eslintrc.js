de982e6ab9d5708cd6f2e518a972f1543511771e
actuarysailor <24359398+actuarysailor@users.noreply.github.com>
2025-08-09 22:57:22 -0400

    ci(units): Add unit tests to validate generating tool

---
+++ .eslintrc.js
@@ -0,0 +1,21 @@
+module.exports = {
+  env: {
+    node: true,
+    es2021: true
+  },
+  extends: [
+    'eslint:recommended'
+  ],
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: 'module'
+  },
+  rules: {
+    'indent': ['error', 2],
+    'linebreak-style': ['error', 'unix'],
+    'quotes': ['error', 'single'],
+    'semi': ['error', 'always'],
+    'no-unused-vars': 'warn',
+    'no-console': 'off'
+  }
+};
